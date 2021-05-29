2ac0aedd3820cf577611add22d74907b0040b26b
ShawnJhang <zzo30802@gmail.com>
2021-05-30 04:23:05 +0800

    [feat] WIP. UDP chat room, TODO: input username

--- chat_room/UDP/src/client.cpp
+++ chat_room/UDP/src/client.cpp
@@ -7,33 +7,33 @@
 #include <iostream>
 #include <string>
 #pragma comment(lib, "ws2_32.lib")
-
+/*
 int main() {
   //-----load winsock2 environment-----
   WSADATA wsa_data;
-  if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
+  if (WSAStartup(MAKEWORD(2, 0), &wsa_data) != 0) {
     std::cout << "[Error] WSAStartup error: " << WSAGetLastError() << std::endl;
     return -1;
   }
   //-----create socket-----
-  SOCKET &&client_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_UDP);
+  SOCKET client_socket = socket(PF_INET, SOCK_DGRAM, 0);
   if (client_socket == INVALID_SOCKET) {
-    std::cout << "[Error]: " << WSAGetLastError() << std::endl;
+    std::cout << "[Error]: socket" << WSAGetLastError() << std::endl;
     return -1;
   }
   //-----bind socket-----
   int len{sizeof(sockaddr_in)};
   sockaddr_in client_address;
   std::memset(&client_address, 0, len);
-  client_address.sin_family = PF_INET;
+  client_address.sin_family = AF_INET;
   client_address.sin_addr.s_addr = inet_addr("127.0.0.1");
   client_address.sin_port = htons(8080);
-  if (bind(client_socket, (SOCKADDR *)&client_address, len) < 0) {
-    std::cout << "[Error]: " << WSAGetLastError() << std::endl;
-    return -1;
-  }
+  // if (bind(client_socket, (SOCKADDR *)&client_address, len) < 0) {
+  //   std::cout << "[Error]: bind" << WSAGetLastError() << std::endl;
+  //   return -1;
+  // }
 
-  char send_msg[100]{0};
+  char send_msg[100];
   int ret{0};
   do {
     std::cout << "Please enter chat content: ";
@@ -42,12 +42,57 @@ int main() {
       std::cout << "Client Socket closed" << std::endl;
       return 0;
     } else {
-      sendto(client_socket, send_msg, sizeof(send_msg) - 1, 0, (SOCKADDR *)&client_address, sizeof(SOCKADDR));
+      int ret = sendto(client_socket, send_msg, sizeof(send_msg), 0, (SOCKADDR *)&client_address, sizeof(SOCKADDR));
+      if (ret == SOCKET_ERROR) {
+        std::cout << "[Error]: sendto" << std::endl;
+      }
     }
-  } while (1);
+  } while (true);
 
   //-----close socket-----
   closesocket(client_socket);
+  //-----clear winsock2的環境-----
+  WSACleanup();
+  return 0;
+}
+*/
+#define BUF_SIZE 100
+int main() {
+  // load winsock2 environment
+  WSADATA wsa_data;
+  if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
+    std::cout << "[Error] WSAStartup error: " << WSAGetLastError() << std::endl;
+    return -1;
+  }
+
+  // create socket
+  SOCKET client_sockfd = socket(PF_INET, SOCK_DGRAM, 0);
+
+  // client address info
+  sockaddr_in server_address;
+  std::memset(&server_address, 0, sizeof(server_address));
+  server_address.sin_family = PF_INET;
+  server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+  server_address.sin_port = htons(8080);
+
+  // input the message and send it to server
+  SOCKADDR client_address;
+  int &&sockarr_size = sizeof(SOCKADDR);
+  while (1) {
+    char buf[BUF_SIZE]{0};
+    std::cout << "Please enter chat content: ";
+    std::cin >> buf;
+    if (strcmp(buf, (char *)("exit")) == 0) {
+      char exit_str[BUF_SIZE]{0};
+      sprintf(exit_str, "The client: %d leaving the char room.", client_sockfd);
+      sendto(client_sockfd, exit_str, BUF_SIZE, 0, (SOCKADDR *)&server_address, sockarr_size);
+      std::cout << "Client Socket closed" << std::endl;
+      return 0;
+    } else
+      sendto(client_sockfd, buf, BUF_SIZE, 0, (SOCKADDR *)&server_address, sockarr_size);
+  }
+  //-----close socket-----
+  closesocket(client_sockfd);
   //-----clear winsock2的環境-----
   WSACleanup();
   return 0;
--- chat_room/UDP/src/server.cpp
+++ chat_room/UDP/src/server.cpp
@@ -9,33 +9,30 @@
 #include <string>
 #include <thread>
 #pragma comment(lib, "ws2_32.lib");  // ws2_32.dll
-// https://blog.csdn.net/CSDN_WHB/article/details/89187866
-// https://www.itread01.com/content/1550247328.html
-DWORD WINAPI thread_receive_client_msg(LPVOID p_client_socket, sockaddr_in &client_addr);
-
+/*
 int main() {
   //-----load winsock2 environment-----
   WSADATA wsa_data;
-  if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
+  if (WSAStartup(MAKEWORD(2, 0), &wsa_data) != 0) {
     std::cout << "[Error] WSAStartup error: " << WSAGetLastError() << std::endl;
     return -1;
   }
   //-----create socket-----
-  SOCKET &&server_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_UDP);
+  SOCKET server_socket = socket(AF_INET, SOCK_DGRAM, 0);
   if (server_socket == INVALID_SOCKET) {
-    std::cout << "[Error]: " << WSAGetLastError() << std::endl;
+    std::cout << "[Error]: socket " << WSAGetLastError() << std::endl;
     return -1;
   }
 
   //-----bind socket-----
   int len{sizeof(sockaddr_in)};
-  sockaddr_in socket_address;
-  std::memset(&socket_address, 0, len);
-  socket_address.sin_family = PF_INET;  // IPv4
-  socket_address.sin_addr.s_addr = inet_addr("127.0.0.1");
-  socket_address.sin_port = htons(8080);
-  if (bind(server_socket, (SOCKADDR *)&socket_address, len) < 0) {
-    std::cout << "[Error]: " << WSAGetLastError() << std::endl;
+  sockaddr_in server_address;
+  std::memset(&server_address, 0, len);
+  server_address.sin_family = AF_INET;  // IPv4
+  server_address.sin_addr.s_addr = INADDR_ANY;
+  server_address.sin_port = htons(8080);
+  if (bind(server_socket, (SOCKADDR *)&server_address, sizeof(SOCKADDR)) == SOCKET_ERROR) {
+    std::cout << "[Error]: bind " << WSAGetLastError() << std::endl;
     return -1;
   }
 
@@ -43,58 +40,68 @@ int main() {
   // UDP不需要listen & accept
   // listen(server_socket, 20);
 
-  //-----loop to listen the clients to connect to the server-----
-  // sockaddr_in client_address;
-  // std::memset(&client_address, 0, len);
-  // std::cout << "Listening for incoming connections." << std::endl;
-  // char recv_msg_from_client[100]{0};
-  // char temp_msg_buf[100]{0};
-  // while (true) {
-  //   // UDP: server don't need to accept client
-  //   recvfrom(server_socket, recv_msg_from_client, 100, 0, (SOCKADDR *)&client_address, (int *)sizeof(SOCKADDR));
-  //   // if socket server receives "exit" from the client and then closes the thread to stop receive msg from the client.
-  //   if ()
-  // }
-
   // 迴圈處理訊息讀取傳送到客戶端
-  sockaddr_in server_address;
-  std::memset(&server_address, 0, len);
-  int len{0};
-  char recv_msg_from_client[100];
-  while ((len = recvfrom(server_socket, recv_msg_from_client, sizeof(recv_msg_from_client) - 1, 0, (SOCKADDR *)&server_address, (int *)sizeof(SOCKADDR))) > 0) {
-    recv_msg_from_client[len] = '\0';  // 在收到訊息最後面加個\0
-    // 將收到的訊息列印出來
-    printf("read [%s:%d]: %s\n", inet_ntoa(server_address.sin_addr), ntohs(server_address.sin_port), recv_msg_from_client);
+  // int recv_msg_len;
+  sockaddr_in client_address;
+  char recv_msg_from_client[100]{0};
+  // std::cout << "sizerecv_msg_from_client: " << <<
+  while (true) {
+    // std::memset(recv_msg_from_client, 0, sizeof(recv_msg_from_client));
+    int recv_msg_len = recvfrom(server_socket, recv_msg_from_client, sizeof(recv_msg_from_client), 0, (SOCKADDR *)&client_address, (int *)sizeof(client_address));
+    // std::cout << "sizeof(recv_msg_from_client): " << sizeof(recv_msg_from_client) << std::endl;
+    // printf("AAAAAAA: %s", recv_msg_from_client);
+    // std::cout << server_socket << " say: " << recv_msg_from_client << std::endl;
+    if (recv_msg_len == SOCKET_ERROR) {
+      // std::cout << "len : " << recv_msg_len << std::endl;
+      continue;
+    } else {
+      std::cout << "len : " << recv_msg_len << std::endl;
+      // recv_msg_from_client[recv_msg_len] = '\0';  // 在收到訊息最後面加個\0
+      // 將收到的訊息列印出來
+      printf("read [%s:%d]: %s\n", inet_ntoa(server_address.sin_addr), ntohs(server_address.sin_port), recv_msg_from_client);
+    }
   }
   // close socket
   closesocket(server_socket);
   // clear the environment of winsock2
   WSACleanup();
 }
+*/
+#define BUF_SIZE 100
+int main() {
+  // load winsock2 environment
+  WSADATA wsa_data;
+  if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
+    std::cout << "[Error] WSAStartup error: " << WSAGetLastError() << std::endl;
+    return -1;
+  }
 
-// DWORD WINAPI thread_receive_client_msg(LPVOID p_client_socket, sockaddr_in &client_addr) {
-//   // initialize array of char
-//   char recv_msg_from_client[100]{0};
-//   char temp_msg_buf[100]{0};
-//   // char send_msg_to_client[100]{0};
-//   // receive data from client
-//   SOCKET client_socket = (SOCKET)p_client_socket;
-//   printf("Welcome %d into the char room.\n", client_socket);
-//   while (1) {
-//     recvfrom(client_socket, recv_msg_from_client, 100, 0, (SOCKADDR *)&client_addr, (int *)sizeof(SOCKADDR));
-//     // if socket server receives "exit" from the client and then closes the thread to stop receive msg from the client.
-//     if (strcmp(recv_msg_from_client, (char *)("exit")) == 0) {
-//       sendto(client_socket, "Client Socket closed.", strlen("Client Socket closed.") + 1, 0, (SOCKADDR *)&client_addr, (int)sizeof(SOCKADDR));
-//       std::cout << client_socket << " leaving the chat room." << std::endl;
-//       break;  // 一旦client離開server就關掉這個thread
-//     }
-//     // print the message from client
-//     sprintf(temp_msg_buf, "%s say: %s", inet_ntoa(client_addr.sin_addr), recv_msg_from_client);
-//     printf("%s\n", temp_msg_buf);
-//   }
-//   // close socket
-//   closesocket(client_socket);
-//   // clear the environment of winsock2
-//   WSACleanup();
-// }
+  // create socket
+  SOCKET server_sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  // bind socket
+  sockaddr_in server_address;
+  std::memset(&server_address, 0, sizeof(server_address));
+  server_address.sin_family = PF_INET;                 // IPv4 address
+  server_address.sin_addr.s_addr = htonl(INADDR_ANY);  // auto to receive IP address
+  server_address.sin_port = htons(8080);
+  bind(server_sockfd, (SOCKADDR *)&server_address, sizeof(SOCKADDR));
+
+  // receive the message from client
+  SOCKADDR client_address;
+  char buf[BUF_SIZE];
+  int &&sockarr_size = sizeof(SOCKADDR);
+  while (1) {
+    int &&str_len = recvfrom(server_sockfd, buf, BUF_SIZE, 0, &client_address, &sockarr_size);
+    if (str_len == SOCKET_ERROR)
+      continue;
+    else {
+      std::cout << client_address.sa_family << " say: " << buf << std::endl;
+    }
+    // sendto(server_sockfd, buf, str_len, 0, &client_address, buf_size);
+  }
+  closesocket(server_sockfd);
+  WSACleanup();
+  return 0;
+}
 #endif  // _SERVER_H_
